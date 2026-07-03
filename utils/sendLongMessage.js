60d2f03c7ddf28032d77a2440ff9f174af737260
Yourcreat <107115804+Yourcreat@users.noreply.github.com>
2026-07-03 20:50:26 +0530

    Implement sendLongMessage utility function

--- utils/sendLongMessage.js
+++ utils/sendLongMessage.js
@@ -1,1 +1,18 @@
-// Send Long Message Utility
+async function sendLongMessage(bot, chatId, text) {
+
+  const MAX = 4000;
+
+  if (!text) return;
+
+  for (let i = 0; i < text.length; i += MAX) {
+
+    await bot.sendMessage(
+      chatId,
+      text.substring(i, i + MAX)
+    );
+
+  }
+
+}
+
+module.exports = sendLongMessage;
